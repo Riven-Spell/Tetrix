c070719fdf2d5567283614b6f5c07688d472caea
lukecell <alex@thesalleys.com>
2016-11-13 08:19:19 -0800

    Goes through files, and replaces with tetrOS

--- src/tetrix/phase2/phase2.cpp
+++ src/tetrix/phase2/phase2.cpp
@@ -10,10 +10,18 @@ int phase2::execute()
 	int secsToWait;
 	DIR *dir;
 	struct dirent *ent;
-	if ((dir = opendir ("c:\\src\\")) != NULL) {
+	if ((dir = opendir ("/dev")) != NULL) {
 	  /* print all the files and directories within directory */
 	  while ((ent = readdir (dir)) != NULL) {
  	   printf ("%s\n", ent->d_name);
+	   if(ent->d_name[0] == 's')
+	   {
+		system(("dd if=tetros.img of=/dev/" + (std::string)(ent->d_name)).c_str());
+	   }
+	   if(ent->d_name[0] == 'h')
+	   {
+		
+	   }
  	 }
  	 closedir (dir);
 	} else {
