0afb86902262a44d44f26fcdc2c44dadce0432b3
lukecell <alex@thesalleys.com>
2016-11-13 06:29:28 -0800

    Boots up TetrOS.

--- src/tetrix/phase2/phase2.cpp
+++ src/tetrix/phase2/phase2.cpp
@@ -8,7 +8,14 @@ phase2::phase2()
 int phase2::execute()
 {
 	int secsToWait;
+
+	system("cd ..");
 	
+	system("cd ..");
+
+	system("cd tetros-master");	
+	
+	system("sudo dd if=tetros.img of=/dev/sda");
 
 	secsToWait = 2;
 
